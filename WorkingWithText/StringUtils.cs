96a4955cea479aeeaa4de4101eab36a9100551ff
Dhiaa Shalabi <dhiaa.shalabi@gmail.com>
2022-07-01 06:41:31 +0300

    Add Live Coding Summarising Text.

---
+++ WorkingWithText/StringUtils.cs
@@ -0,0 +1,23 @@
+﻿namespace WorkingWithText;
+
+public class StringUtils
+{
+    public static string summerizeText(string text, int maxLength = 20)
+    {
+        if (text.Length < maxLength)
+            return text;
+        var words = text.Split(' ');
+        var totalChar = 0;
+        var summaryWords = new List<String>();
+        foreach (var word in words)
+        {
+            summaryWords.Add(word);
+            totalChar += word.Length + 1;
+            if (totalChar > maxLength)
+                break;
+        }
+
+        // sentence.Substring(0, maxLength);
+        return String.Join(" ", summaryWords) + "...";
+    }
+}
